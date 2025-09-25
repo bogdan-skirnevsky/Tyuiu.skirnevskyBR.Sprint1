a155d5c6b627fd3847ce0d0b47b6213226a1bf4c
Богдан Скирневский <tyuiu.skirnevskybr@outlook.com>
2025-09-25 21:59:46 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.skirnevskyBR.Sprint1.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.skirnevskyBR.Sprint1.Task0.V13.Lib;
+namespace Tyuiu.skirnevskyBR.Sprint1.Task0.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.skirnevskyBR.Sprint1.Task0.V13/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.skirnevskyBR.Sprint1.Task0.V13.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Скирневский Б.Р. | ИБКСб-25-1";
+Console.WriteLine("********************************************************************");
+Console.WriteLine("* Спринт #1                                                        *");
+Console.WriteLine("* Тема: Базовые навыки в С#                                        *");
+Console.WriteLine("* Задание #0                                                       *");
+Console.WriteLine("* Вариант #13                                                      *");
+Console.WriteLine("* Выполнил: Скирневский Богдан Романович | ИБКСб-25-1              *");
+Console.WriteLine("********************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                         *");
+Console.WriteLine("* Написать консольную программу, которая вычисляет 24/(6*2)-24/6/4 *");
+Console.WriteLine("* и напечать результат на экране.                                  *");
+Console.WriteLine("********************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                 *");
+Console.WriteLine("********************************************************************");
+Console.WriteLine("* 24/(6*2)-24/6/4                                                  *");
+Console.WriteLine("********************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                       *");
+Console.WriteLine("********************************************************************");
+Console.WriteLine(ds.Calculate());
+Console.ReadLine();
